561066dadf5ee606afe0872cd99e87c9f2979085
Reval Fahmi <reval.fahmi42@gmail.com>
2024-08-13 11:43:31 +0700

    Bytecode run

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+import time
+
+start_time = time.time()
+
 print("Hello")
+print("World")
+print("Hello World")
+
+print("Hello Bos")
+# ini adalah comment
+a = 10 # ini adalah assignment 
+"""ada apa dengan 
+kode ini"""
+print(a)
+print(time.time() - start_time, "detik")
+# cara running code dengan di compile yaitu
+# python -m py_compile [namafile]
