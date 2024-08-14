44513fab37d63b2e7495dbc00f782a716a5707d1
Reval Fahmi <reval.fahmi42@gmail.com>
2024-08-14 13:48:45 +0700

    Variable

---
+++ Variable.py
@@ -0,0 +1,24 @@
+# Variable adalah tempat menyimpan data
+
+# assginment nilai
+a = 10
+b = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai b = ", b)
+print("Nilai panjang = ", panjang)
+
+# penamaan (penamaan variable tidak boleh diawali angka dan special text)
+nilai_y = 15 # dengan menggunakan underscore
+juta10 = 10000000 
+nilaiZ = 17.5
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assignment indirect
+b = a
+print("Nilai b = ", b)
